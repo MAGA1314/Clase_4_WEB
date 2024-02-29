28238b78f291912af3a34b26116a51f54c0089dd
MAGA1314 <garciaagudelomiguelangel@gmail.com>
2024-02-28 21:47:47 -0500

    avance entrega 3

--- JAVASCRIPT/javascript.js
+++ JAVASCRIPT/javascript.js
@@ -57,3 +57,55 @@ numeros.forEach(num =>{
     Para cada arreglo minimo un metodo
     Usando el parametro(Callbackfn)
 */
+
+//Primer Arreglo
+const generarnumeros = (limite) => {
+    const numeros = [];
+    for (let i = 1; i <= limite; i++) {
+        numeros.push(i);
+    }
+    return numeros;
+};
+const generarnumeroshasta5 = generarnumeros(5);
+console.log(generarnumeroshasta5);
+
+// Filtrar los números entre 2 y 4
+const filternum = generarnumeroshasta5.filter((dato) => {
+    return dato > 2 && dato < 4;
+});
+console.log('Números entre 2 y 4:', filternum);
+
+// Mapear arreglo generarnumeros para que saque el seno de cada numero
+let mapeogenerarnumeros = generarnumeroshasta5.map(generarnumeros => {
+    return Math.sin(generarnumeros);
+});
+console.log('Seno de cada numero del arreglo',mapeogenerarnumeros);
+
+
+// Segundo Arreglo
+const generarArregloMixtoAscendente = (inicio, fin) => {
+    return Array.from({ length: fin - inicio + 1 }, (_, index) => `Elemento ${inicio + index}`);
+};
+const arregloMixtoAscendente = generarArregloMixtoAscendente(1, 5);
+console.log('Arreglo mixto ascendente:', arregloMixtoAscendente);
+console.log(arregloMixtoAscendente.filter(datos=>{
+    return datos.startsWith("men");
+}))
+/*
+let string = ["colombia", " brasil", "panama"]
+console.log(string.filter(datos=>{
+    return datos.startsWith("col");
+}));
+console.log(string);
+*/ 
+
+// Tercer Arreglo
+const generarArregloMixtoDescendente = (inicio, fin) => {
+    return Array.from({ length: fin - inicio + 1 }, (_, index) => `Elemento ${fin - index}`);
+};
+const arregloMixtoDescendente = generarArregloMixtoDescendente(1, 5);
+console.log('Arreglo mixto descendente:', arregloMixtoDescendente);
+
+// Cuarto Arreglo
+let generosPeliculas = ["accion", "comedia", "terror","drama"]
+console.log(generosPeliculas)
